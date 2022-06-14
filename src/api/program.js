f8418c1e2cb84243d08ff311fb65bbe80e29ba32
hj <806026398@qq.com>
2022-06-14 09:38:20 +0800

    项目管理部分

---
+++ src/api/program.js
@@ -0,0 +1,23 @@
+import request from '@/utils/request.js';
+export const tableData = async(teamid) => {
+    return request({
+      url: '/volunteerProgramDetails/end/selectByTeamIdANDName',
+      method: 'post',
+      data:{
+        teamid:teamid
+      },
+      baseURL: 'http://192.168.1.147:8088',
+    });
+  };
+  export const search = async(teamid,pid,pname) => {
+    return request({
+      url: '/volunteerProgramDetails/end/selectByTeamIdANDName',
+      method: 'post',
+      data:{
+        teamid:teamid,
+        pid:pid,
+        pname:pname
+      },
+      baseURL: 'http://192.168.1.147:8088',
+    });
+  };
